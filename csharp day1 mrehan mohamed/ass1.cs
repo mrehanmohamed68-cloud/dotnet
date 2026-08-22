0071cf561cda2fdfcf19f78b3dff2b59fe759beb
Mrehan Mohamed <mrehanmohamed68@gmail.com>
2026-08-23 01:42:49 +0300

    Add files via upload

---
+++ csharp day1 mrehan mohamed/ass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+namespace day01
+{
+    class ass1
+    { 
+        public static void Main()
+        {
+           WriteLine("Hello World!");
+        }
+    }
+}
